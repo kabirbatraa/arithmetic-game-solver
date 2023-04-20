de74d62faa8cec685e8357762aa869474b8c9fd2
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-04-20 14:56:44 -0400

    when error occurs then break out of loop

--- main.py
+++ main.py
@@ -39,39 +39,40 @@ start_button.click()
 # repeat until game is over
 while True:
 
-  # sleep for a random time between 0.8 and 1.3 seconds
-  sleep(random.uniform(1.3, 1.8))
+  # try catch everything, and if there is an error, then break out of the loop
+  try:
+    # sleep for a random time between 0.8 and 1.3 seconds
+    sleep(random.uniform(1.3, 1.8))
+
+    # find span with this data:  <span class="problem">74 + 35</span>
+    problem = driver.find_element(By.CSS_SELECTOR, 'span.problem')
+
+    # find input block:  <input class="answer">
+    answer = driver.find_element(By.CSS_SELECTOR, 'input.answer')
+
+    # write the number 0 in answer block
+    # answer.send_keys('0')
+
+    # convert problem.text to a string and replace ÷ division sign with /
+    # also convert – to - and × to *
+    # problemText = str(problem.text).replace('÷', '/')
+    problemText = str(problem.text).replace('÷', '/').replace('–', '-').replace('×', '*')
 
 
-  # if span.problem doesnt exist, then break out of the loop
-  if not driver.find_elements(By.CSS_SELECTOR, 'span.problem'):
+    # read the problem as a string and then execute it as if it is a math expression
+    # then convert to integer and then to string
+    executedString = str(int(eval(problemText)))
+
+    # write the executedString to answer block on character at a time with a small delay between each character
+    for char in executedString:
+      answer.send_keys(char)
+      # sleep for random time between 0.08 and 0.15 seconds
+      sleep(random.uniform(0.08, 0.15))
+      # sleep(0.15)
+  
+  # catch the error and break out of the loop
+  except:
     break
-
-  # find span with this data:  <span class="problem">74 + 35</span>
-  problem = driver.find_element(By.CSS_SELECTOR, 'span.problem')
-
-  # find input block:  <input class="answer">
-  answer = driver.find_element(By.CSS_SELECTOR, 'input.answer')
-
-  # write the number 0 in answer block
-  # answer.send_keys('0')
-
-  # convert problem.text to a string and replace ÷ division sign with /
-  # also convert – to - and × to *
-  # problemText = str(problem.text).replace('÷', '/')
-  problemText = str(problem.text).replace('÷', '/').replace('–', '-').replace('×', '*')
-
-
-  # read the problem as a string and then execute it as if it is a math expression
-  # then convert to integer and then to string
-  executedString = str(int(eval(problemText)))
-
-  # write the executedString to answer block on character at a time with a small delay between each character
-  for char in executedString:
-    answer.send_keys(char)
-    # sleep for random time between 0.08 and 0.15 seconds
-    sleep(random.uniform(0.08, 0.15))
-    # sleep(0.15)
 
 
 
